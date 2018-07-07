3b012538b4cc9421854450a0ff041c46a34804ef
Rob Martens <robert.martens@gmail.com>
2018-07-07 19:02:55 -0400

    Add basic text view
    Defocusing the text view automatically on mouse exit solves the problem of the tab key not cycling anymore after editing. We'll see if this is annoying or not in the long term.

--- Temblor/Controls/Viewport.cs
+++ Temblor/Controls/Viewport.cs
@@ -18,10 +18,11 @@ namespace Temblor.Controls
 
 		private Dictionary<int, string> _viewNames = new Dictionary<int, string>
 		{
-			{ 0, "Tree" },
-			{ 1, "3D Wireframe" },
-			{ 2, "3D Flat" },
-			{ 3, "3D Textured" }
+			{ 0, "Text" },
+			{ 1, "Tree" },
+			{ 2, "3D Wireframe" },
+			{ 3, "3D Flat" },
+			{ 4, "3D Textured" }
 		};
 
 		public string ViewName
@@ -50,6 +51,7 @@ namespace Temblor.Controls
 
 		public Dictionary<int, Control> Views = new Dictionary<int, Control>();
 
+		private TextArea _viewText = new TextArea() { BackgroundColor = Colors.Yellow, TextReplacements = TextReplacements.None };
 		private TreeGridView _viewTree = new TreeGridView() { BackgroundColor = Colors.Cyan };
 		private View3d _view3dWire = new View3d() { ClearColor = new Color4(1.0f, 0.0f, 0.0f, 1.0f) };
 		private View3d _view3dFlat = new View3d() { ClearColor = new Color4(0.0f, 1.0f, 0.0f, 1.0f) };
@@ -63,10 +65,19 @@ namespace Temblor.Controls
 			DropDown.DataStore = _viewNames.Values;
 			DropDown.SelectedValueChanged += (sender, e) => { View = DropDown.SelectedIndex; };
 
-			Views.Add(0, _viewTree);
-			Views.Add(1, _view3dWire);
-			Views.Add(2, _view3dFlat);
-			Views.Add(3, _view3dTex);
+			// To avoid interrupting Tab cycling until users actually want to edit
+			// something, defocus controls by default and focus this Viewport instead.
+			_viewText.Shown += (sender, e) => { Focus(); };
+			_viewText.MouseLeave += (sender, e) => { Focus(); };
+
+			_viewTree.Shown += (sender, e) => { Focus(); };
+			_viewTree.MouseLeave += (sender, e) => { Focus(); };
+
+			Views.Add(0, _viewText);
+			Views.Add(1, _viewTree);
+			Views.Add(2, _view3dWire);
+			Views.Add(3, _view3dFlat);
+			Views.Add(4, _view3dTex);
 
 			LoadComplete += Viewport_LoadComplete;
 		}
